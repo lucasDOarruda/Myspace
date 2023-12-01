1a477a48a8d800030a85adb07d12e91eccd76ebe
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-12-01 18:24:57 +1100

    adding img also section - to home page

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Typography, Button, Grid, Hidden } from '@mui/material';
+
 import { Link } from 'react-router-dom';
 
 
 const Header = () => {
   return (
     <AppBar position="static">
-    <Toolbar>
-      <IconButton size="large" edge="start" color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-      YaData
-      </Typography>
-      <Button color="inherit" component={Link} to="/">Home</Button>
-      <Button color="inherit" component={Link} to="/services">Services</Button>
-      <Button color="inherit" component={Link} to="/about">About</Button>
-      <Button color="inherit" component={Link} to="/portfolio">Portfolio</Button>
-      <Button color="inherit" component={Link} to="/solutions">Solutions</Button>
-    </Toolbar>
-  </AppBar>
-);
+      <Toolbar>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+          <Typography variant="h6" component="div">
+                YaData
+              </Typography>
+            
+            <Hidden mdUp>
+              {/* Show this only on small screens */}
+              
+            </Hidden>
+          </Grid>
+          <Grid item>
+            <Hidden smDown>
+              {/* Show this only on larger screens */}
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                
+              </Typography>
+            </Hidden>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/services">Services</Button>
+            <Button color="inherit" component={Link} to="/about">About</Button>
+            <Button color="inherit" component={Link} to="/portfolio">Portfolio</Button>
+            <Button color="inherit" component={Link} to="/solutions">Solutions</Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
--- pages/Home.js
+++ pages/Home.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import backgroundImage from '../components/18086.jpg';
+import '../Home.css';
 
+function Home() {
+  const sectionStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+    width: '100%'
+  };
 
-function Home  () {
-  return
-   <div>
-
-     <h1>Home Page</h1>
-
-  </div>;
-
-
+  return (
+    <div style={sectionStyle}>
+      <section>
+        <h2 className='mainhometitle'>Transforming Raw Data into Strategic Intelligence</h2>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
