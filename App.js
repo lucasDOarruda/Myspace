e9392fb89d1e427202837fd38efcbe1b6d5e5055
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-12-01 16:33:16 +1100

    adding MUI react nav bar

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import Portfolio from './pages/Portfolio';
 import Solutions from './pages/Solutions';
 
 
+
 function App  ()  {
   return (
     <Router>
--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
+
 
 const Header = () => {
   return (
-    <header className="App-header">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/about">About</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/solutions">Solutions</Link>
-      </nav>
-    </header>
-  );
+    <AppBar position="static">
+    <Toolbar>
+      <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      YaData
+      </Typography>
+      <Button color="inherit" component={Link} to="/">Home</Button>
+      <Button color="inherit" component={Link} to="/services">Services</Button>
+      <Button color="inherit" component={Link} to="/about">About</Button>
+      <Button color="inherit" component={Link} to="/portfolio">Portfolio</Button>
+      <Button color="inherit" component={Link} to="/solutions">Solutions</Button>
+    </Toolbar>
+  </AppBar>
+);
 }
 
 export default Header;
