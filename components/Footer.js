3f1e7cc1b90a4257eeeae441ea83571f232932e8
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-12-01 23:32:48 +1100

    microsfot badge

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import microsoft from '../img/Microsoft.png';
+
+const footerStyle = {
+  position: 'fixed', // or 'absolute' if preferred
+  bottom: '0',
+  width: '100%',
+  backgroundColor: '#f0f0f0', // You can change the background color
+  textAlign: 'center',
+  padding: '5px 0', // Reduced padding
+};
+
+const smallText = {
+  fontSize: '12px', // Smaller font size
+};
 
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} YaData</p>
+    <footer style={footerStyle}>
+      <p style={smallText}>
+        &copy; {new Date().getFullYear()} YaData 
+        <img src={microsoft} alt="Microsoft Gold Partner Logo" style={{ width: '100%' }} />
+      </p>
     </footer>
   );
 }
