bbfb2cb7b43fd95f138872340282c6ccc335d05b
lucasDOarruda <lucas.arrudamiles@icloud.com>
2023-12-09 14:15:14 +1100

    fixing port 6000 to app.py server ..playing with NLP for conversation chat with bot

--- src/app.py
+++ src/app.py
@@ -12,9 +12,13 @@ nlp = spacy.load("en_core_web_sm")
 # Define intents and entities (customize as needed)
 intents = {
     "contact_information": ["contact", "contact info", "get in touch", "how to contact"],
+    "company_information": ["what do you do", "about company", "company info"]
 }
-entities = {
-    "email": ["email", "e-mail", "email address"],
+
+# Define responses for each intent
+responses = {
+    "contact_information": "You can contact us at contact@example.com.",
+    "company_information": "We are a tech company specializing in AI solutions."
 }
 
 # Route to process incoming chat messages
@@ -23,20 +27,21 @@ def process_message():
     user_message = request.json.get('input')
     doc = nlp(user_message.lower())
 
-    # Initialize intent and entity variables
+    # Initialize intent variable
     detected_intent = None
-    detected_entity = None
 
-    # Intent and entity detection logic
-    for token in doc:
-        # Add your intent and entity detection logic here
+    # Intent detection logic
+    for intent, keywords in intents.items():
+        if any(keyword in user_message.lower() for keyword in keywords):
+            detected_intent = intent
+            break
 
-    # Generate a response based on the detected intent and entity
-    bot_response = "Your customized response based on intent and entity."
+    # Generate a response based on the detected intent
+    bot_response = responses.get(detected_intent, "I'm not sure how to respond to that.")
 
     # Return the response to the client
     return jsonify({'botResponse': bot_response})
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=6000)
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,26 +5,24 @@ const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
 
-  // Function to send a message to the server and process the response
   const sendMessage = async () => {
     try {
-      // Ensure that the URL matches the Flask server's URL
-      const response = await axios.post('http://localhost:5000/process_message', { input });
+      const response = await axios.post('http://localhost:6000/process_message', { input });
       const botResponse = response.data.botResponse;
   
-      // Update the messages state with both user and bot messages
+      // Append user and bot messages to the messages state
       setMessages([
         ...messages,
         { text: input, from: 'user' },
         { text: botResponse, from: 'bot' },
       ]);
   
-      // Clear the input field after sending the message
       setInput('');
     } catch (error) {
       console.error('Error sending message:', error);
     }
   };
+  
 
   return (
     <div>
@@ -35,12 +33,7 @@ const Chat = () => {
           </div>
         ))}
       </div>
-      <input 
-        type="text" 
-        value={input} 
-        onChange={(e) => setInput(e.target.value)} 
-        onKeyPress={(e) => e.key === 'Enter' && sendMessage()} 
-      />
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
       <button onClick={sendMessage}>Send</button>
     </div>
   );
